3d183882ce5eac5fae5334dd07f8d0d9501f0938
nbourg8 <nbourg8@users.noreply.github.com>
2017-08-26 10:24:29 -0500

    Add files via upload
    Updated Log Format
    Added ability to log remote command steps

--- Image_Check.bat
+++ Image_Check.bat
@@ -1,5 +1,7 @@
 @echo off
-set LogFile=%1
+set LogFile=c:\Information.txt
+Title "%computername% : Getting Information"
+
 for /f "tokens=3" %%a in ('reg query hkey_users\.default /v imageversion ^|findstr /ri "REG_SZ"') do (set image=%%a)
-for /f "tokens=4" %%a in ('cscript slmgr.vbs /xpr ^|findstr /r "Windows"') do (set activation=%%a)
-echo.%computername%,%image%,%activation%,%date%,%time%>>%LogFile%
+for /f "tokens=4" %%a in ('cscript slmgr.vbs /xpr ^|findstr /r "machine"') do (set activation=%%a)
+echo.%computername%,%image%,%activation%,%date%,%time%>%LogFile%
--- Main.bat
+++ Main.bat
@@ -3,21 +3,77 @@ if not "%username%"=="canrigadmin" (goto failed_username)
 Title Collecting Information
 color 2
 cls
-if not exist "c:\Canrig" mkdir "c:\Canrig"
-if not exist "c:\Canrig\LogFolder" mkdir "c:\Canrig\LogFolder"
-set LogFile="c:\Canrig\LogFolder\Info_Collection.txt"
-echo.********************************************************>>%LogFile% 2>&1
-echo.Current Time is : %Time%
-echo.Current Date is : %Date%
-echo.Log File is in %LogFile%
+set LogFile="c:\Info_Collection.txt"
+echo.********************************************************>%LogFile%
+echo.Current Time is : %Time%>>%LogFile%
+echo.Current Date is : %Date%>>%LogFile%
+echo.>>%LogFile%
+echo.>>%LogFile%
+
+net use x: \\192.168.1.12\C$
 psexec \\192.168.1.12 /c Image_Check.bat %LogFile%
+copy x:\Information.txt %~dp0
+copy /b %LogFile%+Information.txt %LogFile%
+del x:\Information.txt
+net use x: /delete /y
+
+net use x: \\192.168.1.13\C$
 psexec \\192.168.1.13 /c Image_Check.bat %LogFile%
+copy x:\Information.txt %~dp0
+copy /b %LogFile%+Information.txt %LogFile%
+del x:\Information.txt
+net use x: /delete /y
+
+net use x: \\192.168.1.14\C$
 psexec \\192.168.1.14 /c Image_Check.bat %LogFile%
+copy x:\Information.txt %~dp0
+copy /b %LogFile%+Information.txt %LogFile%
+del x:\Information.txt
+net use x: /delete /y
+
+net use x: \\192.168.1.16\C$
 psexec \\192.168.1.16 /c Image_Check.bat %LogFile%
+copy x:\Information.txt %~dp0
+copy /b %LogFile%+Information.txt %LogFile%
+del x:\Information.txt
+net use x: /delete /y
+
+net use x: \\192.168.1.23\C$
 psexec \\192.168.1.23 /c Image_Check.bat %LogFile%
+copy x:\Information.txt %~dp0
+copy /b %LogFile%+Information.txt %LogFile%
+del x:\Information.txt
+net use x: /delete /y
+
+net use x: \\192.168.1.180\C$
 psexec \\192.168.1.180 /c Image_Check.bat %LogFile%
-psexec \\192.168.1.181 /c Image_Check.bat %LogFile%
+copy x:\Information.txt %~dp0
+copy /b %LogFile%+Information.txt %LogFile%
+del x:\Information.txt
+net use x: /delete /y
+
+net use x: \\192.168.1.181\C$
+Rpsexec \\192.168.1.181 /c Image_Check.bat %LogFile%
+copy x:\Information.txt %~dp0
+copy /b %LogFile%+Information.txt %LogFile%
+del x:\Information.txt
+net use x: /delete /y
+
+net use x: \\192.168.1.5\C$
 psexec \\192.168.1.5 /c Image_Check.bat %LogFile%
+copy x:\Information.txt %~dp0
+copy /b %LogFile%+Information.txt %LogFile%
+del x:\Information.txt
+net use x: /delete /y
+
+net use x: \\192.168.1.20\C$
 psexec \\192.168.1.20 /c Image_Check.bat %LogFile%
+copy x:\Information.txt %~dp0
+copy /b %LogFile%+Information.txt %LogFile%
+del x:\Information.txt
+net use x: /delete /y
+
+del Information.txt
+
 Title Finished!
 pause
